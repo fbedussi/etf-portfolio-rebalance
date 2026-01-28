efd9eba5570ac8b7fe5f188fd195acb8ad9bdb4d
Francesco Bedussi <francesco@francescobedussi.it>
2026-01-28 23:29:22 +0100

    fix: drift card order

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,8 @@ export default function App() {
               </div>
               <DataTable />
               <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2">
-                <DriftCardCountry />
                 <DriftCard />
+                <DriftCardCountry />
               </div>
               <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2">
                 <ChartPie />
